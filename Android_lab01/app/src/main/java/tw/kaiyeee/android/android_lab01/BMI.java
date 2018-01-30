89efed5bebe92222dda9131addb1cd876e3f4de7
KaiYi.C <oschangkai@gmail.com>
2018-01-30 16:20:29 +0800

    Finished 80% of android-lab01

---
+++ Android_lab01/app/src/main/java/tw/kaiyeee/android/android_lab01/BMI.java
@@ -0,0 +1,41 @@
+package tw.kaiyeee.android.android_lab01;
+
+/**
+ * Created by KaiYi.C on 30/01/2018.
+ */
+
+public class BMI {
+    private double bmi;
+    private double[] bmiLevel = {18.5, 24, 27, 30, 35};
+    private String[] Suggestions = {
+        "「體重過輕」，需要多運動，均衡飲食，\n以增加體能，維持健康！",
+        "「健康體重」，要繼續保持喔！",
+        "「體重過重」，要小心囉，\n趕快力行健康體重管理吧！",
+        "「輕度肥胖」，\n需要立刻力行健康體重管理喔！",
+        "「中度肥胖」，\n需要立刻力行健康體重管理喔！",
+        "「重度肥胖」，\n需要立刻力行健康體重管理喔！"
+    };
+
+    public void setBmi(double height, double weight) {
+        double h = height / 100;
+        double w = weight;
+        bmi = w / h / h;
+    }
+
+    public double getBmi() { return bmi; }
+    public String getSuggestion() {
+        if(bmi < bmiLevel[0]) {
+            return Suggestions[0];
+        } else if(bmi >= bmiLevel[0] && bmi < bmiLevel[1]) {
+            return Suggestions[1];
+        } else if(bmi >= bmiLevel[1] && bmi < bmiLevel[2]) {
+            return Suggestions[2];
+        } else if(bmi >= bmiLevel[2] && bmi < bmiLevel[3]) {
+            return Suggestions[3];
+        } else if(bmi >= bmiLevel[3] && bmi < bmiLevel[4]) {
+            return Suggestions[4];
+        } else if(bmi >= bmiLevel[4] && bmi < bmiLevel[5]) {
+            return Suggestions[5];
+        } else return "錯誤";
+    }
+}
--- Android_lab01/app/src/main/java/tw/kaiyeee/android/android_lab01/MainActivity.java
+++ Android_lab01/app/src/main/java/tw/kaiyeee/android/android_lab01/MainActivity.java
@@ -2,12 +2,61 @@ package tw.kaiyeee.android.android_lab01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText heightET;
+    private EditText weightET;
+    private TextView bmiTV;
+    private TextView suggestionTV;
+    private BMI bmi = new BMI();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        heightET = findViewById(R.id.heightET);
+        weightET = findViewById(R.id.weightET);
+        bmiTV = findViewById(R.id.bmiTV);
+        suggestionTV = findViewById(R.id.suggestionTV);
+    }
+
+    public void calculateButtonClicked(View v) {
+        boolean isNull = checkIsNull();
+        if(isNull) {
+            Toast.makeText(MainActivity.this, R.string.empty_noti, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        showText();
+    }
+    private boolean checkIsNull() {
+        if(heightET.getText().toString().equals(""))
+            return true;
+        if(weightET.getText().toString().equals(""))
+            return true;
+        if(heightET.getText().toString().equals("0"))
+            return true;
+        if(weightET.getText().toString().equals("0"))
+            return true;
+        return false;
+    }
+    private void showText() {
+        double bmiVal;
+        String suggestion;
+        double height = Double.parseDouble(heightET.getText().toString());
+        double weight = Double.parseDouble(weightET.getText().toString());
+
+        bmi.setBmi(height, weight);
+        bmiVal = bmi.getBmi();
+        suggestion = bmi.getSuggestion();
+
+        bmiTV.setText(String.valueOf(bmiVal));
+        suggestionTV.setText(suggestion);
     }
 }
